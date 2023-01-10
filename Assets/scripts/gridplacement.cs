f89fe9ce12b111c63ac92125e9669768f6a67699
Elliott <dbtracker.dk@gmail.com>
2023-01-10 17:08:00 +0100

    level editor add different blocks

--- Assets/scripts/gridplacement.cs
+++ Assets/scripts/gridplacement.cs
@@ -5,15 +5,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.EventSystems;
+ using UnityEngine.UI;
 
 public class gridplacement : MonoBehaviour
 {
-    public GameObject blockPrefab;
-    public GameObject middlePiece;
+    public List<GameObject> blockPrefabs;
+    public GameObject connector;
     public GameObject cornerPrefab;
     public Vector2 size;
 
     public Transform parent;
+    public Transform connectorParent;
     public Transform cornerParent;
 
     public float zPos;
@@ -29,6 +31,8 @@ public class gridplacement : MonoBehaviour
     public int blockType = 0;
 
     public string MapString;
+
+    public ToggleGroup toggleGroup;
 
 
     int n;
@@ -59,19 +63,9 @@ public class gridplacement : MonoBehaviour
     void Update()
     {
         // blockType += Mathf.RoundToInt(Input.mouseScrollDelta.y);
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                Debug.Log("UI was clicked");
-            }
-            else
-            {
-                Debug.Log("Scene was clicked");
-            }
-        }
 
-        if(Input.GetMouseButton(0)){
+
+        if(Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()){
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
@@ -100,7 +94,7 @@ public class gridplacement : MonoBehaviour
             }
             MapString += ',';
         }
-        Debug.Log(MapString);
+        // Debug.Log(MapString);
         return MapString;
     }
 
@@ -136,27 +130,43 @@ public class gridplacement : MonoBehaviour
         worldPosition.z = zPos;
 
 
+        
+        // for (int i = 0; i < parent.transform.childCount; i++)
+        // {
+        //     Transform child = parent.transform.GetChild(i);
 
-        for (int i = 0; i < parent.transform.childCount; i++)
-        {
-            Transform child = parent.transform.GetChild(i);
+        //     if(child.position == worldPosition){
+        //         return;
+        //     }
 
-            if(child.position == worldPosition){
-                return;
-            }
+        // }
+        List<int> block = new List<int>() {
+                                x, 
+                                y,
+                                blockType,
+                                Rotation
+                            };
+        // foreach (List<int> row in mapList)
+        // {
+        //     Debug.Log("-----------");
+        //     if(row.SequenceEqual(block)) {
+                
+        //         return;
+        //     }
+        // }
 
-        }
+        if(mapList.Any(x => x.SequenceEqual(block))) return;
 
-        mapList.Add(new List<int>() {
-            x,
-            y,
-            blockType,
-            Rotation});
+        mapList.Add(block);
 
         blockPlacements.Add(new Vector2(x, y));
 
         MapString = exportMapAsString();
-
+        
+        // 0 -1 0 0 ,0 -2 0 0 ,0 -3 0 0 ,-2 -2 0 0 ,-1 -2 0 0 ,1 -2 0 0 ,2 -2 0 0 ,2 -3 0 0 ,2 -4 0 0 ,2 -5 0 0 ,1 -5 0 0 ,0 -5 0 0 ,-1 -5 0 0 ,-2 -5 0 0 ,-2 -4 0 0 ,-2 -3 0 0 ,-1 -3 0 0 ,1 -3 0 0 ,1 -4 0 0 ,0 -4 0 0 ,-1 -4 0 0 ,0 -6 0 0 ,0 -7 0 0 ,0 -8 0 0 ,2 -8 0 0 ,1 -8 0 0 ,-1 -8 0 0 ,-2 -8 0 0 ,-2 -9 0 0 ,-1 -9 0 0 ,0 -9 0 0 ,1 -9 0 0 ,2 -9 0 0 ,3 -8 0 0 ,3 -9 0 0 ,-3 -8 0 0 ,-3 -9 0 0 ,-4 -9 0 0 ,-5 -9 0 0 ,-6 -9 0 0 ,-4 -8 0 0 ,-6 -10 0 0 ,-6 -11 0 0 ,-6 -12 0 0 ,4 -9 0 0 ,4 -10 0 0 ,5 -10 0 0 ,4 -11 0 0 ,4 -12 0 0 ,5 -11 0 0 ,5 -12 0 0 ,-2 -10 0 0 ,-1 -10 0 0 ,-1 -11 0 0 ,-1 -12 0 0 ,-1 -13 0 0 ,-2 -12 0 0 ,-2 -11 0 0 ,-2 -13 0 0 ,0 -4 0 0 ,0 -4 0 0 ,0 -4 0 0 ,0 -4 0 0 ,0 -4 0 0 ,0 -4 0 0 ,
+        if(blockType != 0) worldPosition += Vector3.back * 2;
+        GameObject newObject = Instantiate(blockPrefabs[blockType], worldPosition, Quaternion.identity, parent);
+        if(blockType != 0) return;
         for (int i = 0; i < parent.transform.childCount; i++)
         {
             Transform child = parent.transform.GetChild(i);
@@ -164,10 +174,10 @@ public class gridplacement : MonoBehaviour
             foreach (Vector3 side in sides)
             {
                 if(worldPosition + side == child.position){
-                    Instantiate(middlePiece, worldPosition + side/2 + Vector3.back, Quaternion.identity, parent);
+                    Instantiate(connector, worldPosition + side/2 + Vector3.back, Quaternion.identity, connectorParent);
                 }
                 
-                if(side.x != 0){
+                else if(side.x != 0){
                     foreach(Vector3 Yside in Ysides){
                         if(worldPosition + side + Yside == child.position){
                             // Debug.Log(worldPosition + side + Yside);
@@ -187,6 +197,11 @@ public class gridplacement : MonoBehaviour
             
 
             // if(child.)
-        GameObject newObject = Instantiate(blockPrefab, worldPosition, Quaternion.identity, parent);
+    }
+
+    public void updateSelectedItem(){
+        Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        Debug.Log(toggle.transform.GetSiblingIndex());
+        blockType = toggle.transform.GetSiblingIndex();
     }
 }
