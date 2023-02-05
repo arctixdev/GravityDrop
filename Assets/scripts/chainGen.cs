49e16fc0275393a8fb9e83eac7a56c28694d2e5c
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-02-05 15:52:34 +0100

    chain building in chain test scene

---
+++ Assets/scripts/chainGen.cs
@@ -0,0 +1,64 @@
+
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chainGen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject chain;
+
+    public int chainLength;
+
+    public float DistancePerChain;
+
+    [SerializeField]
+    bool lockFront;
+    [SerializeField]
+    bool lockEnd;
+
+    Rigidbody2D endrb;
+    void Start()
+    {
+        genChain(transform.position);
+    }
+
+    void genChain(Vector3 startPos){
+        GameObject NewChain = Instantiate(chain, startPos, Quaternion.identity, transform);
+        NewChain.transform.GetChild(0).GetComponent<HingeJoint2D>().connectedBody = null;
+        NewChain.transform.GetChild(0).GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+        NewChain.transform.GetChild(1).GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+        Rigidbody2D rb = NewChain.transform.GetChild(1).GetComponent<Rigidbody2D>();
+        for (int i = 1; i < chainLength; i++)
+        {
+            NewChain = Instantiate(chain, startPos + Vector3.down * i * DistancePerChain * transform.localScale.y, Quaternion.identity, transform);
+            NewChain.transform.GetChild(0).GetComponent<HingeJoint2D>().connectedBody = rb;
+            NewChain.transform.GetChild(0).GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+            NewChain.transform.GetChild(1).GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+
+            rb = NewChain.transform.GetChild(1).GetComponent<Rigidbody2D>();
+
+
+        }
+        if(lockEnd){
+            endrb = transform.GetChild(transform.childCount - 1).GetChild(1).GetComponent<Rigidbody2D>();
+            endrb.constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.gameObject.transform.GetChild(0).GetChild(0).position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Vector3 mspos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // endrb.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        transform.GetChild(transform.childCount - 1).GetChild(1).position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 4);
+
+        if(Input.GetMouseButtonDown(0)){
+            genChain(Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 4);
+        }
+        
+
+    }
+}
