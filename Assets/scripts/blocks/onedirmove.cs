1dca7d3e78694ad52d791da9892adadcbb74d691
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-07-01 21:44:19 +0200

    tried fixng 1 dir movebles to work when rotated
    did not work

---
+++ Assets/scripts/blocks/onedirmove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onedirmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.constraints = transform.rotation.z % 180 < 45 ? RigidbodyConstraints2D.FreezePositionX : RigidbodyConstraints2D.FreezePositionY;
+    }
+}
