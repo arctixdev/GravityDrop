f1e25813e54d5b4719e8055cadb1f596253e3a83
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-07-03 12:06:01 +0200

    fixed lvl2

--- Assets/scripts/blocks/onedirmove.cs
+++ Assets/scripts/blocks/onedirmove.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class onedirmove : MonoBehaviour
 {
+    public float z;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.constraints = transform.rotation.z % 180 < 45 ? RigidbodyConstraints2D.FreezePositionX : RigidbodyConstraints2D.FreezePositionY;
+        float rot = transform.eulerAngles.z;
+        if(rot < 0){
+            rot += 360;
+        }
+        rb.constraints = (rot % 180 > 45 ? RigidbodyConstraints2D.FreezePositionX : RigidbodyConstraints2D.FreezePositionY) | RigidbodyConstraints2D.FreezeRotation;
+
+    }
+    void Update()
+    {
+        z = transform.eulerAngles.z;
     }
 }
