98c685add1f59e7497e0e7086b8ff82f268e3ed0
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-01-20 14:19:00 +0100

    comment

--- Assets/scripts/mainController.cs
+++ Assets/scripts/mainController.cs
@@ -225,6 +225,7 @@ public class mainController : MonoBehaviour
             rotNew = rot < 0 ? rot + 360 : rot;
             if(curRot != rotNew)
             {
+                // Update everything if the rotation has happened
                 curRot = rotNew;
                 thudScipt.rotate();
                 StartCoroutine(shiftGrav());
