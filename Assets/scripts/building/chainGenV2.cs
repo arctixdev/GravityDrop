05a1ab2ad35e1fe5e1c8a2e842b4f64e38f78ede
Qusika2 <75113441+Enc0dedClass@users.noreply.github.com>
2024-02-03 10:51:41 +0100

    idk eather (broken i think)

--- Assets/scripts/building/chainGenV2.cs
+++ Assets/scripts/building/chainGenV2.cs
@@ -1,32 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
+using UnityEngine.Rendering;
+using UnityEngine.EventSystems;
 
 public class chainGenV2 : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 y = Input.mousePosition;
+            Vector2[] x =
+            {
+                y,
+                y + new Vector2(0, -1),
+                y + new Vector2(0, -2),
+                y + new Vector2(0, -3)
+            };
+            generateChain(x, 100);
+        }
+    }
+
     void generateChainSegment()
     {
 
     }
+
+    [SerializeField] private bool generateDebugLines;
     [SerializeField] private float chainSegmentLength;
-    public bool generateChain(Vector2[] points, int precision, int searchDistance = 3)
+    //public bool generateChain(Vector2[] points, int precision, int searchDistance = 3)
+    public bool generateChain(Vector2[] points, int precision)
     {
-        if(points.Length < 2) return false;
-        Vector2[] computedPoints = bezeirCurve.PointList2(new List<Vector2>(points), 1 / precision).ToArray();
-        int aproxemateChainSegments = 1000;
-        bool shifter = true;
-        for(int i = 0; i < aproxemateChainSegments; i++)
+        if (EventSystem.current.IsPointerOverGameObject()) return false;
+
+        if (points.Length < 2) return false;
+        Vector2[] computedPoints = bezeirCurve.PointList2(new List<Vector2>(points), true, precision).ToArray();
+
+        if(generateDebugLines)
         {
-            (int, float) min = new(-1, -1f);
-            for(int j = 0; j < searchDistance; j++)
+            for (int i = 1; i <= computedPoints.Length; i++)
             {
-                if(i+j < computedPoints.Length)
-                {
-                    float dis = Vector2.Distance(computedPoints[i], computedPoints[i + j]);
-                    if (Mathf.Abs(chainSegmentLength - dis) < min.Item2) min = new(i-j, dis);
-                }
+                DrawLine(computedPoints[i - 1], computedPoints[i]);
             }
         }
         return true;
+    }
+
+    [SerializeField] private Material material;
+    void DrawLine(Vector3 start, Vector3 end)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        LineRenderer lr = myLine.AddComponent<LineRenderer>();
+        lr.material = material;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        //GameObject.Destroy(myLine, duration);
     }
 }
--- Assets/scripts/utils/math/bezeirCurve.cs
+++ Assets/scripts/utils/math/bezeirCurve.cs
@@ -1,27 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class bezeirCurve : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public static Vector2 Point2(float t, List<Vector2> controlPoints)
     {
         int N = controlPoints.Count - 1;
         if (N > 16)
         {
-            Debug.Log("You have used more than 16 control points. The maximum control points allowed is 16.");
+            UnityEngine.Debug.Log("You have used more than 16 control points. The maximum control points allowed is 16.");
             controlPoints.RemoveRange(16, controlPoints.Count - 16);
         }
 
@@ -38,35 +30,94 @@ public class bezeirCurve : MonoBehaviour
 
         return p;
     }
-    public static List<Vector2> PointList2(
-      List<Vector2> controlPoints,
-      float interval = 0.01f)
+    public static Vector2 Point2(float t, List<Vector2> controlPoints, float[] arcLengths, bool evenlySpaced)
     {
-        int N = controlPoints.Count - 1;
-        if (N > 16)
+        if(!evenlySpaced) return Point2(t, controlPoints);
+
+        UnityEngine.Debug.Log("setting up starting values");
+        float len = arcLengths[arcLengths.Length - 1];
+        float targetLength = t * len;
+        int low = 0, high = arcLengths.Length - 1, index = 0;
+        UnityEngine.Debug.Log("initiating binary search");
+        //while (low < high)
+        //{
+        //    index = low + (((high - low) / 2) | 0);
+        //    if (arcLengths[index] < targetLength)
+        //    {
+        //        low = index + 1;
+        
+        //    }
+        //    else
+        //    {
+        //        high = index;
+        //    }
+        //}
+        if (arcLengths[index] > targetLength)
         {
-            Debug.Log("You have used more than 16 control points. " +
-              "The maximum control points allowed is 16.");
-            controlPoints.RemoveRange(16, controlPoints.Count - 16);
+            index--;
         }
 
+        UnityEngine.Debug.Log("binary seach found index " + index);
+        float lengthBefore = arcLengths[index];
+
+        if (lengthBefore == targetLength)
+        {
+            return Point2(index / len, controlPoints);
+
+        }
+        else
+        {
+            return Point2((index + (targetLength - lengthBefore) / (arcLengths[index + 1] - lengthBefore)) / len, controlPoints);
+        }
+    }
+
+    public static List<Vector2> PointList2(List<Vector2> controlPoints, int pointAmount)
+    {
+        int interval = 1 / pointAmount;
         List<Vector2> points = new List<Vector2>();
         for (float t = 0.0f; t <= 1.0f + interval - 0.0001f; t += interval)
         {
-            Vector2 p = new Vector2();
-            for (int i = 0; i < controlPoints.Count; ++i)
-            {
-                Vector2 bn = Bernstein(N, i, t) * controlPoints[i];
-                p += bn;
-            }
-            points.Add(p);
+            points.Add(Point2(t, controlPoints));
         }
 
         return points;
     }
-
-    private static float Bernstein(int n, int i, float t)
+    public static List<Vector2> PointList2(List<Vector2> controlPoints, bool evenlySpaced, int pointAmount, int precision = 100)
     {
+        float interval = 1 / pointAmount;
+        float[] arcLenths = new float[precision];
+        UnityEngine.Debug.Log("beginning arclength generation");
+        float counter = Time.realtimeSinceStartup;
+        arcLenths[0] = 0f;
+        for(int i = 1; i < precision; i++)
+        {
+            float index = 1 / precision * i;
+            arcLenths[i] = arcLenths[i - 1] + Vector2.Distance(Point2(index - 1, controlPoints), Point2(index, controlPoints));
+        }
+        UnityEngine.Debug.Log("finished arclength generation in " + (Time.realtimeSinceStartup - counter) + "seconds");
+        UnityEngine.Debug.Log("beginning point generation");
+        counter = Time.realtimeSinceStartup;
+        List<Vector2> points = new List<Vector2>();
+        int it = 0;
+        for (float t = 0.0f; t <= 1.0f && it < 500; t += interval)
+        {
+            //Process currentProcess1 = Process.GetCurrentProcess();
+            //currentProcess1.Refresh();
+            //long totalMemoryUsedByProcess1 = currentProcess1.WorkingSet64;
+            //if(totalMemoryUsedByProcess1 > 10_000_000_000) break;
+
+            //if (t > interval * 500) break;
+
+            points.Add(Point2(t, controlPoints, arcLenths, true));
+            it++;
+        }
+        UnityEngine.Debug.Log("finished arclength generation in " + (Time.realtimeSinceStartup - counter) + "seconds with " + it + " iterations");
+
+        return points;
+    }
+
+private static float Bernstein(int n, int i, float t)
+{
         float t_i = Mathf.Pow(t, i);
         float t_n_minus_i = Mathf.Pow((1 - t), (n - i));
 
