6cea96824124e167a59329a3845c952131513773
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-02-24 13:04:58 +0100

    make offset tilemaps work in test scene

--- Assets/scripts/mapHandling/tileMapHandler.cs
+++ Assets/scripts/mapHandling/tileMapHandler.cs
@@ -8,94 +8,26 @@ public class tileMapHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     public Tilemap grid;
+    public Tilemap collisonGrid;
 
-    public List<Tile> outSideTiles = new List<Tile>();
-    public List<Tile> inSideTiles = new List<Tile>();
-    public List<Tile> sqareTiles = new List<Tile>();
-    void Start()
-    {
-        
-    }
+    public TileBase ruleTile;
+    public Tile collisonTile;
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        changeBlock(2, 2, true);
     }
 
     public void changeBlock(int BX, int BY, bool place){
-        
-        for (int x = 0; x < 4; x++)
-        {
-            for (int y = 0; y < 4; y++)
-            {
-                updateTile((BX * 2) + x - 2, (BY * 2) + y - 2, BX, BY, place);
-                // placeTile(x, y, outSideTiles);
-            }
-        }
-    }
+        TileBase tile = place ? ruleTile : null;
+        int x = BX * 2;
+        int y = BY * 2;
+        grid.SetTile(new Vector3Int(x, y), tile);
+        grid.SetTile(new Vector3Int(x + 1, y), tile);
+        grid.SetTile(new Vector3Int(x, y + 1), tile);
+        grid.SetTile(new Vector3Int(x + 1, y + 1), tile);
 
-    bool isCornerTile(Tile tile){
-        return inSideTiles.Contains(tile) || tile == null;
-    }
-
-    public void removeBlock(){
-
-    }
-
-    bool checkTile(int x, int y, int BX, int BY, bool place){
-        // Debug.Log(x - (x % 2));
-        if(x - BX * 2 is -1 or 0 && y - BY * 2 is -1 or 0) return place;
-        return !isCornerTile(grid.GetTile<Tile>(new Vector3Int(x, y, 0)));
-        // return true;
-    }
-
-    void updateTile(int x, int y, int BX, int BY, bool place){
-        int TilesAround = 0;
-
-        
-        if(checkTile(x + -1, y, BX, BY, place)) TilesAround ++;
-        if(checkTile(x + 1, y, BX, BY, place)) TilesAround ++;
-        if(checkTile(x, y + -1, BX, BY, place)) TilesAround ++;
-        if(checkTile(x, y + 1, BX, BY, place)) TilesAround ++;
-        
-
-
-        if(!checkTile(x, y, BX, BY, place)){
-            switch (TilesAround)
-            {
-                case <= 1:
-                    removeTile(x, y);
-                    break;
-                case >= 2:
-                    // Debug.Log(x + (x % 2 * 2 - 1) + " : " + x);
-                    // placeTile(x - (x % 2 * 2 - 1), y - (y % 2 * 2 - 1), inSideTiles);
-                    // if(checkTile(x - (x % 2 * 2 - 1), y - (y % 2 * 2 - 1), BX, BY, place)) 
-                    placeTile(x, y, inSideTiles);
-                    break;
-            }
-            return;
-        }
-        switch (TilesAround){
-            case >= 3:
-                placeTile(x, y, sqareTiles);
-                break;
-            default:
-                placeTile(x, y, outSideTiles);
-                break;
-            
-        }
-        return;
-
-
-
-    }
-
-    void placeTile(int x, int y, List<Tile> tiles){
-        grid.SetTile(new Vector3Int(x, y, 0), tiles[3 - (Math.Abs(x) % 2 + (1 - Math.Abs(y) % 2) * 2)]);
-    }
-    void removeTile(int x, int y){
-        grid.SetTile(new Vector3Int(x, y, 0), null);
+        collisonGrid.SetTile(new Vector3Int(x, y), place ? collisonTile : null);
     }
 
     public void clearMap(){
